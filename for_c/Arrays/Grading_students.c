7fc5a1139f9f9f0b759fff8d99da2b6238f86104
Clown Monster <53732385+ClownMonster@users.noreply.github.com>
2019-10-21 18:00:48 +0530

    update2

--- for_c/Arrays/Grading_students.c
+++ for_c/Arrays/Grading_students.c
@@ -3,11 +3,11 @@
 
 int* gradingStudents(int grades_count, int* grades, int* result_count) {
     
-   static int my_grades[10];
+   static int my_grades[10];  // static allocation of the array so the values cna be used even atfer the variables go out of scope
     for(int i = 0;  i< grades_count ; i++)
     {
-        if(grades[i] < 38) my_grades[i] = grades[i];
-        else if((grades[i] % 5)==0) my_grades[i] = grades[i];
+        if(grades[i] < 38) my_grades[i] = grades[i]; // condition for the marks less than 38 and assigning the same grade
+        else if((grades[i] % 5)==0) my_grades[i] = grades[i]; // conndition ffor other than 38.. cheking whether the grade is divisible by 5
         else
          {
              int num = grades[i];
